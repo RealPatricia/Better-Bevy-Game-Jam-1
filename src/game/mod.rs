9adba9d903875ad5890614b1d206aac09c333f89
patricia fargo <pfargo1701@gmail.com>
2022-02-28 00:24:34 -0600

    Init, now with comments

---
+++ src/game/mod.rs
@@ -0,0 +1,12 @@
+// Import app for the PluginGroupBuilder
+use bevy::{prelude::*, app::*};
+
+pub struct GamePlugins;
+
+impl PluginGroup for GamePlugins
+{
+    fn build(&mut self, group: &mut PluginGroupBuilder)
+    {
+
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,31 @@
+/*
+Welcome to this untitled game jam game.It probably won't be ready 
+in time to be able to submit, but it was a fun project that I 
+learned a lot from, and hopefully you'll be able to learn something
+from it as well, especially if I do my job right and properly comment
+everything.
+*/
+
+#![feature(derive_default_enum)]
+
+//Hopefully this one is obvious
+use bevy::prelude::*;
+
+/*
+probably a bit excessive, but I like bundling up all of my game
+code into a single PluginGroup to make everything prettier.
+*/
+mod game;
+use game::*;
+
+fn main()
+{
+    // You can thank C# for the drop brace btw. I know I do.
+    
+    // Setting up the app with the default plugins and our game plugins
+
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(GamePlugins)
+        .run();
+}
